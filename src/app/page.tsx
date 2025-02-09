03992f2a11960e12b12f95df0fa97b2c2f0c6c6e
Bereket Semagn <semagnbereket@gmail.com>
2025-02-09 14:30:11 -0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,9 +25,9 @@ export default function Home() {
           <li>• {age} yrs old</li>
           <li>• software engineer</li>
           <li>• currently in toronto, canada 🇨🇦</li>
-          <li>• born in kenya, family from ethiopia, raised in mississauga</li>
-          <li>• black belt in taekwondo</li>
-          <li>• started coding when i was 10 years old</li>
+          <li>• ethopian 🇪🇹, born in kenya 🇰🇪, raised in canada 🇨🇦</li>
+          <li>• moved to canada when i was 4 yrs old 🇨🇦</li>
+          <li>• started coding when i was 10 years old 💻</li>
           <li>
             • was in{" "}
             <a href="https://www.ibo.org/" target="_blank" rel="noreferrer">
@@ -36,6 +36,7 @@ export default function Home() {
             , but i dropped out to pursue entrepreneurship
           </li>
           <li>• 26k+ followers on twitter</li>
+          <li>• black belt in taekwondo 🥋</li>
         </ul>
 
         <br />
@@ -68,7 +69,7 @@ export default function Home() {
 
         <b>where i&apos;ve worked:</b>
         <ul>
-          <li>• swe intern @ shopify when i was 13 years old</li>
+          <li>• swe intern @ shopify (joined at 13 yrs old)</li>
           <li>• founding engineer @ tunnel (neo &apos;23)</li>
           <li>• founding engineer @ aviato</li>
         </ul>
@@ -78,7 +79,7 @@ export default function Home() {
         <b>what ive done:</b>
         <ul>
           <li>• product hunt maker of the year (runner up) &apos;21</li>
-          <li>• 100k+ monthly active users across all my apps</li>
+          <li>• 250k+ monthly active users across all my apps</li>
           <li>
             • started a community called buildergroop in &apos;21, over 7k+
             members now
